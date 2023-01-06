8e55fef27cc3ea353c1b92c977d7ee137bfe8106
AnkitaPunse <punseankita018@gmail.com>
2023-01-06 18:48:04 +0530

    committing Sample test into Sub Branch

---
+++ src/test/java/ai/ds/apps/testLayer/SampleTest.java
@@ -0,0 +1,24 @@
+package ai.ds.apps.testLayer;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	
+  @Test
+public void Sample() {
+	  
+	  int a = 10; 
+	  int b= 20;
+	  
+	  int c= a+b;
+	  
+	 System.out.println(c); 
+	  
+	
+	
+}
+
+	
+
+}
